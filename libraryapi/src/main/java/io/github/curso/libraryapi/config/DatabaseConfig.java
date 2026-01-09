3118899d86a618c5f7d62d7aa12d5d7dbee9cbbe
VitoriaBarbosa42 <vitoria42.barbosa@gmail.com>
2026-01-09 14:23:36 -0300

    Iniciando curso

---
+++ libraryapi/src/main/java/io/github/curso/libraryapi/config/DatabaseConfig.java
@@ -0,0 +1,26 @@
+package io.github.curso.libraryapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String dirver;
+
+//    @Bean
+//    public DataSource dataSoucer(){
+//        DriverManagerDataSource
+//    }
+}
